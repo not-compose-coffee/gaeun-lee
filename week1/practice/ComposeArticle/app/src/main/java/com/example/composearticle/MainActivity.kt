f8042f193f32c6102450404e67a9dda92f8a0c83
gaeun <geun5744@gmail.com>
2023-04-17 13:47:23 +0900

    [Add] Compose Article

--- week1/practice/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
+++ week1/practice/ComposeArticle/app/src/main/java/com/example/composearticle/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -20,11 +18,17 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.composearticle.ui.theme.ComposeArticleTheme
+import kotlin.math.log
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { }
+        setContent { ArticleCard(
+            title = stringResource(id = R.string.title_jetpack_compose_tutorial),
+            shortDescription = stringResource(id = R.string.compose_short_desc),
+            longDescription = stringResource(id = R.string.compose_long_desc),
+            imagePainter = painterResource(id = R.drawable.bg_compose_background)
+        )}
     }
 }
 
@@ -39,10 +43,25 @@ private fun ArticleCard(
     imagePainter: Painter,
     modifier: Modifier = Modifier,
 ) {
-    Column() { }
+    Column(modifier=modifier) {
+        Image(painter = imagePainter, contentDescription = null, modifier = Modifier.fillMaxWidth())
+        Text(text = title,
+        fontSize = 24.sp,
+        modifier = Modifier.padding(all = 16.dp))
+        Text(text = shortDescription,modifier=Modifier.padding(start = 16.dp, end = 1.dp), textAlign = TextAlign.Justify)
+        Text(text = longDescription,modifier=Modifier.padding(all=16.dp), textAlign = TextAlign.Justify)
+    }
 }
 
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() { }
+fun DefaultPreview() {
+    ArticleCard(
+        title = stringResource(id = R.string.title_jetpack_compose_tutorial),
+        shortDescription = stringResource(id = R.string.compose_short_desc),
+        longDescription = stringResource(id = R.string.compose_long_desc),
+        imagePainter = painterResource(id = R.drawable.bg_compose_background)
+    )
+
+}
